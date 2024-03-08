4cd195ac20066a7db1384874eac31e4913849b13
ColeDragonborn <74426554+matthewwcole@users.noreply.github.com>
2024-03-08 11:59:48 -0500

    Refactored

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,31 +7,58 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Breakfast',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
+      appBar: appBar(),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Breakfast',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+
+        },
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg'
+          ),
+      ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+
+          },
+        child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        child: SvgPicture.asset(
+          'assets/icons/dots.svg'
+          ),
+      ),
         ),
-      ),
-      backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Color(0xffF7F8F8),
-              borderRadius: BorderRadius.circular(10)
-            ),
-          child: SvgPicture.asset(
-            'assets/icons/Arrow - Left 2.svg'
-            
-            ),
-        )
-      ),
+      ],
     );
   }
 }
